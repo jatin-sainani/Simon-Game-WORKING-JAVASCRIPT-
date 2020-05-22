6df908c08568a85fcc165ce4ca148a47e862beb1
Rachit Agarwal <rachitagarwal80@gmail.com>
2020-05-22 18:07:48 +0530

    Add files via upload

---
+++ SimonGame/game.js
@@ -0,0 +1,85 @@
+var buttonColors=["red","blue","green","yellow"];
+var gamePattern=[];
+var userClickedPattern=[];
+var level=0;
+var flag=true;
+function nextSequence(){
+        var randomNumber=Math.floor(Math.random()*4);
+        var randomChosenColor=buttonColors[randomNumber];
+        gamePattern.push(randomChosenColor);
+        $("#"+randomChosenColor).fadeIn(100).fadeOut(100).fadeIn(100);
+        playSound(randomChosenColor);
+        level++;
+        $("h1").text("Level "+level);
+        
+        userClickedPattern=[];
+        
+}   
+ 
+$(".btn").on("click",function(e){
+        var userChosenColor= e.target.id;
+        console.log(userChosenColor);
+        userClickedPattern.push(userChosenColor);
+        console.log(userClickedPattern);
+        console.log(gamePattern);
+        
+        playSound(userChosenColor);
+        animatePress(userChosenColor);
+        checkAnswer(userClickedPattern.length-1);
+    });
+
+    
+function playSound(name){   
+        var audio1= new Audio("sounds/"+name+".mp3");
+        audio1.play();
+}
+
+function animatePress(currentColor){
+        $("#"+currentColor).addClass("pressed");
+        setInterval(function(){
+        $("#"+currentColor).removeClass("pressed");
+    },100)
+}
+
+$(document).on("keypress",function(){
+        if(flag===true){
+            nextSequence();
+        }
+        flag=false;
+});
+function gameOverSound(){
+    var gameOver=new Audio("sounds/wrong.mp3");
+    gameOver.play();
+}
+function checkAnswer(currentLevel){
+    
+        
+            if(userClickedPattern[currentLevel]===gamePattern[currentLevel]){
+                if(userClickedPattern.length===gamePattern.length){
+                        setTimeout(function(){
+                            nextSequence();
+                        },1000);
+                    }
+                }
+                else{
+                    $("h1").text("Game Over - Press any key to restart");
+                    $("body").addClass("game-over");
+                    setTimeout(function(){
+                        $("body").removeClass("game-over");
+                    },200);
+                    gameOverSound(); 
+                   $(document).on("keypress",function(){
+                    if(flag==false){
+                        startOver();
+                    }
+                   });
+                }
+
+  
+}
+function startOver(){
+    level=0;
+    gamePattern=[];
+    nextSequence();
+   flag=true;
+}
